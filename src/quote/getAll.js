5a6ac209f53542deb88008f7dc23065306d94d1d
Alexandre LEROY <alexandre.wonkledge.leroy@gmail.com>
2020-06-14 13:31:00 +0200

    add devis_ids

--- src/quote/getAll.js
+++ src/quote/getAll.js
@@ -4,6 +4,7 @@ import {query} from "@wonkledge/okatsu/lib/mongooseAdapter";
 import Quote from "./model";
 import {mapFields} from "@wonkledge/okatsu/lib/datamapper";
 import {checkParameters} from '@wonkledge/okatsu/lib/validator';
+import {isUuid} from "@wonkledge/okatsu/lib/validator/built-in";
 
 const allStatus = ['ongoing', 'accepted', 'refused', 'closed'];
 
@@ -14,18 +15,35 @@ const mapping = [
     }
 ];
 
+const checkDevisIds = devis_ids => {
+    const devisIdsArr = devis_ids.split(',');
+
+    return devisIdsArr.reduce( (acc,val) => {
+        return acc && isUuid(val);
+    }, true)
+}
+
 const validators = [
     {
         field: 'status',
         predicate: status => allStatus.includes(status),
         errorMessage: 'id must have uuid format',
         required: false
+    },
+    {
+        field: 'devis_ids',
+        predicate: checkDevisIds,
+        errorMessage: 'devis_id must be array of uuids',
+        required: false
     }
 ];
 
 const fetchQuote = (params) => {
-    console.log(params.status);
-    const searchCondition = params.status ? { 'state.status': params.status } : {};
+    console.log(params);
+    let searchCondition = params.status ? { 'state.status': params.status } : {};
+    searchCondition = params.devis_ids ? { 'public_id': { $in: params.devis_ids.split(',')}} : searchCondition
+
+
     return Quote.find(searchCondition);
 };
 
